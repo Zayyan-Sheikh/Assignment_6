57fdeea7ebd93573fb0bae7a51479e0826805239
Zayyan Sheikh <muhammadzayyan2526@gmail.com>
2024-12-12 11:36:53 +0500

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import Image from 'next/image';
+import React from 'react';
 import Header from '@/Components/Header/header';
 import Footer from '@/Components/Footer/footer';
 import Navbar from '@/Components/Navbar/navbar';
