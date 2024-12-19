7c82028679266249e9ba001dc170dc12926faaf9
Zayyan Sheikh <muhammadzayyan2526@gmail.com>
2024-12-19 09:33:35 +0500

    this

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Header from '@/Components/Header/header';
-import Footer from '@/Components/Footer/footer';
-import Navbar from '@/Components/Navbar/navbar';
-import HeroSection from '@/Components/HeroSection/hero';
-import CourseSection from '@/Components/CourseSection/course';
-import Achievement from '@/Components/Achievements/achievement';
-import Courses from '@/Components/Courses/courses';
-import TeamSection from '@/Components/TeamSection/team';
-import CustomerTestimonial from '@/Components/CustomerTestimonial/customerTestimonial';
+import Header from '@/app/Components/Header/header';
+import Footer from '@/app/Components/Footer/footer';
+import Navbar from '@/app/Components/Navbar/navbar';
+import HeroSection from '@/app/Components/HeroSection/hero';
+import CourseSection from '@/app/Components/CourseSection/course';
+import Achievement from '@/app/Components/Achievements/achievement';
+import Courses from '@/app/Components/Courses/courses';
+import TeamSection from '@/app/Components/TeamSection/team';
+import CustomerTestimonial from '@/app/Components/CustomerTestimonial/customerTestimonial';
 
 export default function Home() {
   return (
